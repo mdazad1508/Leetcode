5bff0c8766b1e0136ca8b492c131f49a6123c577
Azad <mdazad15081999@gmail.com>
2023-02-13 20:19:30 +0530

    Added solution - LeetHub

---
+++ Reverse Level Order Traversal - GFG/reverse-level-order-traversal.java
@@ -0,0 +1,168 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GFG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+        while(t > 0){
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Tree g = new Tree();
+            ArrayList<Integer> ans = g.reverseLevelOrder(root);
+
+            for (Integer val: ans) 
+                System.out.print(val+" "); 
+            System.out.println();
+            t--;
+        }
+    }
+}
+
+
+ 
+
+// } Driver Code Ends
+
+
+
+/* 
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+
+class Tree
+{
+    public ArrayList<Integer> reverseLevelOrder(Node node) 
+    {
+        // code here
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        for(int i =height(node);i>=0;i--){
+            NodeAtK(node,i,ans);
+        }
+        
+        return ans;
+    }
+    
+    
+    static void NodeAtK(Node root,int  k, ArrayList<Integer> list){
+        
+        
+        if(root==null) return;
+        
+        if(k==0){
+            list.add(root.data);
+            return;
+        }
+        
+        
+        NodeAtK(root.left,k-1,list);
+        NodeAtK(root.right,k-1,list);
+    }
+    
+    static int height(Node root){
+        
+        if(root==null)  return 0;
+        
+        
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        
+        return Math.max(leftHeight, rightHeight) +1;
+    }
+}      
